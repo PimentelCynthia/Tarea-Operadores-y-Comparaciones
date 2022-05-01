c813cbb4adf2d2d42277928f13e0c7bf9eafde86
PimentelCynthia <cynthia.fpim@gmail.com>
2022-04-30 20:43:47 -0500

    se adjunta captura de pantalla en carpeta img

---
+++ assets/js/tareaOperaciones.js
@@ -0,0 +1,37 @@
+alert ("Estoy vivo!!!!");
+
+var A=5;
+var B=3;
+var C=-12;
+
+console.log(" a) A es mayor que 3:", A>3);
+
+console.log(" b) A es mayor que C:", A>C);
+
+console.log(" c) A es menor que C:", A<C);
+
+console.log(" d) B es menor que C:", B<C);
+
+console.log(" e) B es diferente de C:", B!=C);
+
+console.log(" f) A es similar o igual que 3:", A==3);
+
+console.log(" g) A*B es similar o igual a 15:", A*B==15);
+
+console.log(" h) A*B es similar o igual a -30:", A*B== -30);
+
+console.log(" i) C entre B es menor que A:", C/B<A);
+
+console.log(" j) C entre B es similar o igual a -10:", C/B == -10);
+
+console.log(" k) C entre B es similar o igual a -4:", C/B == -4);
+
+console.log(" l) A más B más C es similar o igual a 5:", A+B+C==5);
+
+console.log(" m) La siguiente expresion (A+B==8) && (A-B==2)es:", (A+B==8) && (A-B==2));
+
+console.log(" n) La siguiente expresion (A+B==8) || (A-B==6)es:", (A+B==8) || (A-B==6));
+
+console.log(" o) La siguiente expresion A>3 && B>3 && C<3 es:", A>3 && B>3 && C<3);
+
+console.log(" p) La siguiente expresion A>3 && B>=3 && C<-3 es:", A>3 && B>=3 && C<-3);
